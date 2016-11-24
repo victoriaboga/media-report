0f068ee20e780c043b37aac783c4aae9733d9f96
jahglow <jahglow@gmail.com>
2016-11-24 21:45:57 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,69 @@
-/**
- * Created by IvanP on 07.09.2016.
- */
+require('./main.css');
 
-import Calculator from "./Calculator"
+export default class HeaderScroller{
+  constructor(win,header){
+    this.header = header;
+//TODO: add functionality to dynamically add classes to body and header to make them float off the page.
+    this._meta = {
+      lastScrollY:0,
+      ticking:false,
+      offsetHeight:header.offsetHeight,
+      window:win
+    };
+    win.document.querySelector('body').style.marginTop =  header.offsetHeight + "px";
+
+    win.requestAnimationFrame = win.requestAnimationFrame || win.mozRequestAnimationFrame || win.webkitRequestAnimationFrame || win.msRequestAnimationFrame;
+
+    win.addEventListener("scroll", ()=>{this.scrollFixed()}, false); // attach a resize listener to resize the header
+    win.addEventListener("scroll", ()=>{this.resizeFixed()}, false); // attach a resize listener to resize the header
+  }
+
+  requestTick(callback){
+    if(!this._meta.ticking) {
+      requestAnimationFrame(callback);
+      this._meta.ticking = true;
+    }
+  }
+
+  /**
+   * Calculates widths for all columns in the fixed header based on the `source`
+   * */
+  resizeFixed(){
+    this.requestTick(()=>{
+      this._meta.offsetHeight = this.header.offsetHeight;
+      this._meta.ticking=false;
+      this.scrollFixed(); // to compensate top offset in case after resize the table is less in height and top has changed
+    })
+  }
+
+  /**
+   * Displays a fixed header when the table header is scrolled off the screen
+   * */
+  scrollFixed() {
+    //console.log(this._meta.window);
+    this._meta.lastScrollY = this._meta.window.pageYOffset;
+    this.requestTick(this.constructor._scrollCallback.bind(this));
+  }
+
+  static _scrollCallback(){
+    let offset = this._meta.lastScrollY,
+        offsetTop = this._meta.offsetHeight;
+      this.header.style.top=-offset+'px';
+    this._meta.ticking=false;
+  }
+}
+
+window.addEventListener('DOMContentLoaded',()=>{
+  let r2 = document.querySelector('.r2dashboard');
+  if(r2){
+    r2.addEventListener('load',(e)=>{
+      console.log('r2 iframe loaded');
+      let r2window = e.target.contentWindow;
+      let rHeader = document.querySelector('.reportalBar');
+      if(rHeader){
+        let r2d2 = new HeaderScroller(r2window,rHeader);
+        console.log(r2d2)
+      }
+    })
+  }
+});
