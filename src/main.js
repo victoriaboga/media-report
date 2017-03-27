e31c317d7b6f0bd842653955d18d2c83aa229003
Ivan Pilyugin <jahglow@gmail.com>
2017-03-27 10:45:56 +0300

    fixed error in CSS polyfill

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 require('./main.css');
 import MDHierarchy from "./hierarchy/hierarchy";
-import MenuTransformer from "./navigation/menu-transformer";
+//import MenuTransformer from "./navigation/menu-transformer";
 
 window.addEventListener('DOMContentLoaded',()=>{
   window.Reportal = window.Reportal || {};
   window.Reportal.state = window.Reportal.state || {};
   let state = window.Reportal.state;
   state.hierarchy = new MDHierarchy(); //setup listeners for hierarchy component on open and scrim click
-  state.menuTransformer = new MenuTransformer(); //setup listeners for hierarchy component on open and scrim click
+//  state.menuTransformer = new MenuTransformer(); //setup listeners for hierarchy component on open and scrim click
 });
