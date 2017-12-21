63f9c40a4ce7d2ac768ae365684b845412e34df9
EkaterinaT <Murzik17>
2017-12-21 21:07:47 +0300

    Add scripts for Symptom Overview Page Import these scripts in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 require('./main.css');
+
 import MDHierarchy from "./hierarchy/hierarchy";
 import TransformMenuAt from "./navigation/menu-transformer";
 import SideNav from './side-nav/side-nav';
+
+import './somc-symptom-overview-page-scripts/hierarchy-table-updates';
 
 window.Reportal = window.Reportal || {};
 window.Reportal.TransformMenuAt = TransformMenuAt;
